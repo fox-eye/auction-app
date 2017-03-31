b3183dff3e826153221a47f1ed117ce5e8af1354
Lucas Romano <lucas.romano@ngahr.com>
2017-03-31 15:36:06 +0200

    finish chapter 2

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import ApplicationComponent from '. /components/application/application';
-import CarouselComponent from "./components/carousel/carousel";
+import ApplicationComponent from './components/application/application';
+import CarouselComponent from './components/carousel/carousel';
 import FooterComponent from './components/footer/footer';
 import NavbarComponent from './components/navbar/navbar';
 import ProductItemComponent from './components/product-item/product-item';
